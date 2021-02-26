ec4015ce234692227c7d9f105f9c698c71b5abfd
Idriss Neumann <idriss.neumann@comwork.io>
2021-02-26 13:30:13 +0100

    Improve deployments

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -3,5 +3,5 @@
 REPO_PATH="/home/centos/kompose-api/"
 
 cd "${REPO_PATH}" && git pull origin main || :
-docker rm -rf kompose-api_cmd-api_1 || :
+docker rm -rf kompose-api_kompose-api_1 || :
 docker-compose -f docker-compose-comwork.yml up -d
