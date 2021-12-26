5167657190321f501ad640ade33c3ccb055f82c4
Idriss Neumann <neumann.idriss@gmail.com>
2021-12-26 13:31:23 +0000

    Update deploy.sh

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,7 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-REPO_PATH="${PROJECT_HOME}/kompose-api/"
+docker-compose -f docker-compose-comwork.yml up -d --force-recreate
 
-cd "${REPO_PATH}" && git pull origin main || :
-docker rm -f kompose-api_kompose-api_1 || :
-docker-compose -f docker-compose-comwork.yml up -d
