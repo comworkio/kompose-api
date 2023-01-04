d70e0b37f4bf1e3d4d74d7df24025b333dad1262
Idriss Neumann <idriss.neumann@comwork.io>
2023-01-04 16:12:43 +0100

    Remove some mirrors and add dockerignore

--- ci/mirror.sh
+++ ci/mirror.sh
@@ -5,6 +5,4 @@ REPO_PATH="${PROJECT_HOME}/kompose-api/"
 cd "${REPO_PATH}" && git pull origin main || :
 git push github main 
 git push pgitlab main
-git push bitbucket main
-git push froggit main
 exit 0
